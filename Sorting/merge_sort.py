581cc00c16b281428e31af178e4c49aa4dab7dc3
Shah-Alam-EEE <salamebd01@gmail.com>
2024-01-23 01:06:05 +0600

    Modify merge_sort

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,28 +1,28 @@
-# def merge(left,right):
-#     result=[]
-#     i=j=0
+def merge(left,right):
+    result=[]
+    i=j=0
 
-#     while i<len(left) and j<len(right):
-#         if left[i]>right[j]:
-#             result.append(right[j])
-#             j+=1
-#         else:
-#             result.append(left[i])
-#             i+=1
-#     result+=left[i:]
-#     result+=right[j:]  
-#     return result      
-# def merge__sort(n):
-#     if len(n)<=1:
-#         return n
-#     mid=int(len(n)/2)
-#     left=merge__sort(n[:mid])
-#     right=merge__sort(n[mid:])
-#     return merge(left,right)
+    while i<len(left) and j<len(right):
+        if left[i]>right[j]:
+            result.append(right[j])
+            j+=1
+        else:
+            result.append(left[i])
+            i+=1
+    result+=left[i:]
+    result+=right[j:]  
+    return result      
+def merge__sort(n):
+    if len(n)<=1:
+        return n
+    mid=int(len(n)/2)
+    left=merge__sort(n[:mid])
+    right=merge__sort(n[mid:])
+    return merge(left,right)
 
-# lst=[1,7,3,58,5,6,1]
-# x=merge__sort(lst)
-# print(x)
+lst=[1,7,3,58,5,6,1]
+x=merge__sort(lst)
+print(x)
 
 
 #space complexity is O(n)
