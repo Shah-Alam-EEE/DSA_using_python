35f289aa47ca15a840067fe40ca4e302e537e448
Shah-Alam-EEE <salamebd01@gmail.com>
2024-01-24 19:34:34 +0600

    Add base case of bubble sort

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,12 +1,14 @@
 def Bubble_sort(k):
     n=len(k)
+    is_sorted=1
     for i in range(n-1):
         for j in range(n-1-i):
             if k[j]>k[j+1]:    
                 k[j],k[j+1]=k[j+1],k[j]
-                 
+                is_sorted=0
 
-
+        if is_sorted==1:
+            break
 
 f = [3, 2, 4, 1]
 Bubble_sort(f)
